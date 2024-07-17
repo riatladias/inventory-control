4a7f99280417046788eaa0af6804d7250be3d973
Riatla Dias <riatla.d@gmail.com>
2024-07-16 22:10:37 -0300

    add: mensagem de feedback

--- src/main/java/br/com/springboot/controller/ClienteController.java
+++ src/main/java/br/com/springboot/controller/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/clientes")
@@ -27,19 +28,17 @@ public class ClienteController {
         return new ModelAndView("/cliente/formulario", model);
     }
 
-    // salva os dados no banco de dados
-    // @ModelAttribute Cliente cliente -> monta um obj do tipo cliente vindo do formulário
-    // BindingResult ->
+    // salvar atualizar cadastrar
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String salva(@Valid @ModelAttribute Cliente cliente, BindingResult result) {
-        if (result.hasErrors())
-            return "cliente/formulario";
-
+    public String salva(@Valid @ModelAttribute Cliente cliente, BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) return "cliente/formulario";
         if (cliente.getId() != null) {
             clienteBO.atualiza(cliente);
-            return "redirect:/clientes";
+            attr.addFlashAttribute("feedback", "Cliente foi atualizado com sucesso!");
+        } else {
+            clienteBO.insere(cliente);
+            attr.addFlashAttribute("feedback", "Cliente foi cadastrado com sucesso!");
         }
-        clienteBO.insere(cliente);
         return "redirect:/clientes";
     }
 
@@ -59,17 +58,19 @@ public class ClienteController {
 
     // Inativar cliente
     @RequestMapping(value = "/inativa/{id}", method = RequestMethod.GET)
-    public String inativa(@PathVariable Long id, ModelMap model) {
+    public String inativa(@PathVariable Long id, ModelMap model, RedirectAttributes attr) {
         Cliente cliente = clienteBO.pesquisaPeloId(id);
         clienteBO.inativa(cliente);
+        attr.addFlashAttribute("feedback", "Cliente foi inativado com sucesso!");
         return "redirect:/clientes";
     }
 
     // Ativar cliente
     @RequestMapping(value = "/ativa/{id}", method = RequestMethod.GET)
-    public String ativa(@PathVariable Long id, ModelMap model) {
+    public String ativa(@PathVariable Long id, ModelMap model, RedirectAttributes attr) {
         Cliente cliente = clienteBO.pesquisaPeloId(id);
         clienteBO.ativa(cliente);
+        attr.addFlashAttribute("feedback", "Cliente foi ativado com sucesso!");
         return "redirect:/clientes";
     }
 }
